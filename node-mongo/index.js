ee5a80606560f9366df50864066c4a664919944b
joseleii <Phambaodu25072001@gmail.com>
2021-01-16 20:42:32 +0700

    Node MongoDB Example 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -2,6 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 //MongoClient: connect to the mongoDB server
 
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion'; //created conFusion db
@@ -12,22 +13,44 @@ MongoClient.connect(url, (err, client) => {
     console.log('Connected correctly to server');
 
     const db = client.db(dbname); //connect to db
-    const collection = db.collection('dishes');
+    // const collection = db.collection('dishes');
 
-    collection.insertOne({"name": "Uthapizza", "description": "test"}, (err, result) => {
-        assert.equal(err, null);
-        console.log('After Insert:\n');
-        console.log(result.ops); //how many succesful operation have been done
-        collection.find({}).toArray((err, docs) => {
-            //print all documents 
-            assert.equal(err, null);
-            console.log('Found:\n');
-            console.log(docs); //print all documents that match the criteria
+    // collection.insertOne({"name": "Uthapizza", "description": "test"}, (err, result) => {
+    //     assert.equal(err, null);
+    //     console.log('After Insert:\n');
+    //     console.log(result.ops); //how many succesful operation have been done
+    //     collection.find({}).toArray((err, docs) => {
+    //         //print all documents 
+    //         assert.equal(err, null);
+    //         console.log('Found:\n');
+    //         console.log(docs); //print all documents that match the criteria
 
-            db.dropCollection('dishes', (err, result) => {
-                //clean up the database
-                assert.equal(err, null);
-                client.close(); //close the connection to the database
+    //         db.dropCollection('dishes', (err, result) => {
+    //             //clean up the database
+    //             assert.equal(err, null);
+    //             client.close(); //close the connection to the database
+    //         });
+    //     });
+    // });
+
+    dboper.insertDocument(db, {name: "Vadonut", description: 'Test'}, 'dishes', (result) => {
+        console.log('Insert Document:\n', result.ops); //result.ops number of operation
+        
+        dboper.findDocuments(db, 'dishes', (docs) => {
+            console.log('Found Documents:\n', docs);
+            
+            dboper.updateDocument(db, {name: 'Vadonut'}, {description: 'Updated Test'}, 'dishes', (result) => {
+                console.log('Updated Document:\n', result.result);
+                
+                dboper.findDocuments(db, 'dishes', (docs) => {
+                    console.log('Found Documents:\n', docs);
+                    
+                    db.dropCollection('dishes', (result) => {
+                        console.log('Dropped Collection: ', result);
+                        
+                        client.close();
+                    });
+                });
             });
         });
     });
---
+++ node-mongo/operations.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+
+exports.insertDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection); //look for collection
+    coll.insert(document, (err, result) => {
+        assert.equal(err, null); //if an error occur then print and exit
+        console.log("Inserted " + result.result.n 
+            + " documents into the collection " + collection);
+            //result.n: number of results
+        callback(result); //implement in the index.js file
+    });
+};
+
+exports.findDocuments = (db, collection, callback) => {
+    //find all documents
+    const coll = db.collection(collection);
+    coll.find({}).toArray((err, docs) => {
+        assert.equal(err, null);
+        callback(docs);
+    });
+};
+
+exports.removeDocument = (db, document, collection, callback) => {
+    //callback when operations complete
+    const coll = db.collection(collection);
+    coll.deleteOne(document, (err, result) => {
+        assert.equal(err, null);
+        console.log("Removed the document ", document);
+        callback(result);
+    });
+};
+
+exports.updateDocument = (db, document, update, collection, callback) => {
+    const coll = db.collection(collection);
+    coll.updateOne(document, { $set: update }, null, (err, result) => {
+        assert.equal(err, null);
+        console.log("Updated the document with ", update);
+        callback(result);
+    });
+};
