e4ecbdcfb4b9af30042e663259c53c94eaf16a68
joseleii <Phambaodu25072001@gmail.com>
2021-01-17 12:07:43 +0700

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,32 +8,55 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    // var newDish = Dishes({
+    //     name: 'Uthappizza',
+    //     description: 'test'
+    // });
+    // newDish.save()
+    //     .then((dish) => {
+    //         console.log(dish); // save the newDish
+
+    //         return Dishes.find({}).exec(); 
+    //         //find all the dishes. exec: ensure this is executed, return a promise
+    //         //that promise can be returned so it can be chained the method to the remaining ones
+    //         //return Dishes.find({}); 
+    //     })
+    //     .then((dishes) => {
+    //         console.log(dishes);
+
+    //         return Dishes.remove({});
+    //     })
+    //     .then(() => {
+    //         return mongoose.connection.close();
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
+
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish); // save the newDish
+
+        return Dishes.find({}).exec(); 
+        //find all the dishes. exec: ensure this is executed, return a promise
+        //that promise can be returned so it can be chained the method to the remaining ones
+        //return Dishes.find({}); 
+    })
+    .then((dishes) => {
+        console.log(dishes);
+
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish); // save the newDish
-
-            return Dishes.find({}).exec(); 
-            //find all the dishes. exec: ensure this is executed, return a promise
-            //that promise can be returned so it can be chained the method to the remaining ones
-            //return Dishes.find({}); 
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    
 });
 
 //creating a new dish
