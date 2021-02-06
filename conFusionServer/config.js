a6f67e7f7c903b3ea56e00e55ffa9314959a5983
joseleii <Phambaodu25072001@gmail.com>
2021-02-06 11:45:50 +0700

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -4,7 +4,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '',
-        clientSecret: ''
+        clientId: '670475900275105',
+        clientSecret: 'bf6d5848356804b4be38fa8d7d72bd35'
     }
 }
